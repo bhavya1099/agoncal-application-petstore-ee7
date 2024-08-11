2d5e50330ec0ae77f910d12cbc3116d7761a593d
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-08-11 23:41:43 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ src/test/java/org/agoncal/application/petstore/model/Categories.java
@@ -0,0 +1,81 @@
+package org.agoncal.application.petstore.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getStreet1 {
+
+	}
+
+	interface setStreet1 {
+
+	}
+
+	interface getStreet2 {
+
+	}
+
+	interface setStreet2 {
+
+	}
+
+	interface getCity {
+
+	}
+
+	interface setCity {
+
+	}
+
+	interface getState {
+
+	}
+
+	interface setState {
+
+	}
+
+	interface getZipcode {
+
+	}
+
+	interface setZipcode {
+
+	}
+
+	interface getCountry {
+
+	}
+
+	interface setCountry {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface hashCode {
+
+	}
+
+	interface toString {
+
+	}
+
+}
