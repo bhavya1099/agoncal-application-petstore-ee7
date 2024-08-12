27f490e85d197a6b7c7e74a1b7384775ca597988
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-08-12 19:02:30 +0530

    Unit test generated by RoostGPT
    Using AI Model claude-3-5-sonnet-20240620

---
+++ src/test/java/org/agoncal/application/petstore/model/Categories.java
@@ -0,0 +1,145 @@
+package org.agoncal.application.petstore.model;
+ public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getStreet1 {
+
+	}
+
+	interface setStreet1 {
+
+	}
+
+	interface getStreet2 {
+
+	}
+
+	interface setStreet2 {
+
+	}
+
+	interface getCity {
+
+	}
+
+	interface setCity {
+
+	}
+
+	interface getState {
+
+	}
+
+	interface setState {
+
+	}
+
+	interface getZipcode {
+
+	}
+
+	interface setZipcode {
+
+	}
+
+	interface getCountry {
+
+	}
+
+	interface setCountry {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface hashCode {
+
+	}
+
+	interface toString {
+
+	}
+
+	interface getId {
+
+	}
+
+	interface setId {
+
+	}
+
+	interface getVersion {
+
+	}
+
+	interface setVersion {
+
+	}
+
+	interface getName {
+
+	}
+
+	interface setName {
+
+	}
+
+	interface getDescription {
+
+	}
+
+	interface setDescription {
+
+	}
+
+	interface getIsoCode {
+
+	}
+
+	interface setIsoCode {
+
+	}
+
+	interface getPrintableName {
+
+	}
+
+	interface setPrintableName {
+
+	}
+
+	interface getIso3 {
+
+	}
+
+	interface setIso3 {
+
+	}
+
+	interface getNumcode {
+
+	}
+
+	interface setNumcode {
+
+	}
+
+ interface getCreditCardNumber{}
+}
