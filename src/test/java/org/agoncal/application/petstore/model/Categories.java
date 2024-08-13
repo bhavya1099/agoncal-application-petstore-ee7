921166ac9e769f6c76fe270ae033548cfec933e8
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-08-13 15:41:14 +0530

    Unit test generated by RoostGPT
    Using AI Model claude-3-5-sonnet-20240620

---
+++ src/test/java/org/agoncal/application/petstore/model/Categories.java
@@ -0,0 +1,33 @@
+package org.agoncal.application.petstore.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface hashCode {
+
+	}
+
+	interface toString {
+
+	}
+
+}
