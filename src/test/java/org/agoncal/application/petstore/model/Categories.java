2d0d14e01fba6bcdbda70e5f4c06777d34e254ca
bhavya1099 <bhavya.goswami99@gmail.com>
2024-11-27 15:55:52 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-3.5-turbo

---
+++ src/test/java/org/agoncal/application/petstore/model/Categories.java
@@ -0,0 +1,237 @@
+package org.agoncal.application.petstore.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getStreet1 {
+
+	}
+
+	interface getStreet2 {
+
+	}
+
+	interface getCity {
+
+	}
+
+	interface getState {
+
+	}
+
+	interface getZipcode {
+
+	}
+
+	interface getCountry {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface hashCode {
+
+	}
+
+	interface toString {
+
+	}
+
+	interface getId {
+
+	}
+
+	interface getVersion {
+
+	}
+
+	interface getName {
+
+	}
+
+	interface getDescription {
+
+	}
+
+	interface getIsoCode {
+
+	}
+
+	interface getPrintableName {
+
+	}
+
+	interface getIso3 {
+
+	}
+
+	interface getNumcode {
+
+	}
+
+	interface getCreditCardNumber {
+
+	}
+
+	interface getCreditCardType {
+
+	}
+
+	interface getCreditCardExpDate {
+
+	}
+
+	interface calculateAge {
+
+	}
+
+	interface digestPassword {
+
+	}
+
+	interface update {
+
+	}
+
+	interface getLogin {
+
+	}
+
+	interface getRole {
+
+	}
+
+	interface getUuid {
+
+	}
+
+	interface getPassword {
+
+	}
+
+	interface getFirstName {
+
+	}
+
+	interface getLastName {
+
+	}
+
+	interface getFullName {
+
+	}
+
+	interface getTelephone {
+
+	}
+
+	interface getEmail {
+
+	}
+
+	interface getDateOfBirth {
+
+	}
+
+	interface getAge {
+
+	}
+
+	interface getHomeAddress {
+
+	}
+
+	interface getImagePath {
+
+	}
+
+	interface getUnitCost {
+
+	}
+
+	interface getProduct {
+
+	}
+
+	interface getSubTotal {
+
+	}
+
+	interface getQuantity {
+
+	}
+
+	interface getItem {
+
+	}
+
+	interface getCategory {
+
+	}
+
+	interface getOrderDate {
+
+	}
+
+	interface getTotalWithoutVat {
+
+	}
+
+	interface getVatRate {
+
+	}
+
+	interface getVat {
+
+	}
+
+	interface getTotalWithVat {
+
+	}
+
+	interface getDiscountRate {
+
+	}
+
+	interface getDiscount {
+
+	}
+
+	interface getTotal {
+
+	}
+
+	interface getCustomer {
+
+	}
+
+	interface getOrderLines {
+
+	}
+
+	interface getDeliveryAddress {
+
+	}
+
+	interface getCreditCard {
+
+	}
+
+}
