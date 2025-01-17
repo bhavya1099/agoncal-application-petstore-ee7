535fb22e6f24f7f42195c581fcbd351bd15de5b7
bhavya1099 <bhavya.goswami99@gmail.com>
2025-01-17 13:45:50 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ src/test/java/org/agoncal/application/petstore/model/Categories.java
@@ -0,0 +1,25 @@
+package org.agoncal.application.petstore.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getCreditCardNumber {
+
+	}
+
+}
