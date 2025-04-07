da835d741956c4974be728f8666cfde81e78090c
bhavya1099 <kinshu.1019@gmail.com>
2025-04-07 08:21:09 +0000

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/org/agoncal/application/petstore/rest/Categories.java
@@ -0,0 +1,41 @@
+package org.agoncal.application.petstore.rest;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface create {
+
+	}
+
+	interface deleteById {
+
+	}
+
+	interface findById {
+
+	}
+
+	interface listAll {
+
+	}
+
+	interface update {
+
+	}
+
+}
